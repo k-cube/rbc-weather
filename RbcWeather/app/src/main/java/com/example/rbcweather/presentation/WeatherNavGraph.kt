9620ff0f7e1b37606bd5393526f43b25983fb170
k-cube <keivan.noroozi@gmail.com>
2025-07-22 18:27:02 -0400

    init commit

---
+++ RbcWeather/app/src/main/java/com/example/rbcweather/presentation/WeatherNavGraph.kt
@@ -0,0 +1,12 @@
+package com.example.rbcweather.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph
+
+@Composable
+fun NavGraph(
+    modifier: Modifier = Modifier) {
+
+
+}
