1671f972df5201169ac47e9e4ecf4fbac531ab2c
k-cube <keivan.noroozi@gmail.com>
2025-07-25 13:07:59 -0400

    write tests

--- RbcWeather/app/src/main/java/com/example/rbcweather/domain/Response.kt
+++
@@ -1,6 +0,0 @@
-package com.example.rbcweather.domain
-
-sealed class Response<out T> {
-    data class Success<out T>(val data: T) : Response<T>()
-    data class Error(val error: String) : Response<Nothing>()
-}
--- RbcWeather/app/src/main/java/com/example/rbcweather/domain/repository/WeatherRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.rbcweather.domain.repository
-
-import com.example.rbcweather.domain.Response
-import com.example.rbcweather.domain.WeatherEntity
-import kotlinx.coroutines.flow.Flow
-
-interface WeatherRepository {
-    suspend fun getWeather(lat: Double, long: Double): Flow<Response<WeatherEntity>>
-}
